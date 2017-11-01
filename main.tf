80c0303f4275029d491a19e62efa666367f7e7c9
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-11-01 18:25:19 +0200

    Add NAT module

--- main.tf
+++ main.tf
@@ -4,13 +4,19 @@ provider "aws" {
 #    access_key = "${var.aws_access_key}"
 #    secret_key = "${var.aws_secret_key}"
 }
-
+resource "aws_key_pair" "auth" {
+  key_name   = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
 module "vpc" {
   source = "./modules/vpc"
 }
-# module "nat" {
-#   source = "./modules/nat"
-# }
+module "nat" {
+  source = "./modules/nat"
+  vpc_id = "${module.vpc.vpc_id}"
+  subnet-id = "${module.vpc.subnet-id}"
+  count_number = "${module.vpc.count_number}"
+}
 # module "server" {
 #   source = "./modules/server"
 # }
---
+++ modules/nat/variables.tf
@@ -0,0 +1,19 @@
+## NAT VARIABLES TF
+
+variable "vpc_id" {}
+variable "subnet-id" {
+  type = "list"
+}
+variable "key_name" {
+  default = "id_rsa1"
+}
+variable "count_number" {}
+variable "vpc_net_prefix" {
+  default = "10.0."
+}
+variable "vpc_net_postfix" {
+  default = ".0/24"
+}
+variable "vpc-private-net" {
+  default = "-private-"
+}
